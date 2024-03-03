dc1453417b815dc7b65b0921750286fd13225c8b
Alaeddine-Nasri <ia_nasri@esi.dz>
2024-03-03 23:19:02 +0100

    Committing changes before updating

---
+++ src/app/light-chart/light-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LightChartComponent } from './light-chart.component';
+
+describe('LightChartComponent', () => {
+  let component: LightChartComponent;
+  let fixture: ComponentFixture<LightChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LightChartComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(LightChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/light-chart/light-chart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-light-chart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './light-chart.component.html',
+  styleUrl: './light-chart.component.css'
+})
+export class LightChartComponent {
+
+}
